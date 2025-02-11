38dc4527ab17a46b8a32bffdb735d161d9778f47
Nauclhlt <rei.20080312@gmail.com>
2025-02-11 12:31:07 +0900

    Updated cs rolling hash.

--- rolling_hash.cs
+++ rolling_hash.cs
@@ -1,124 +1,166 @@
-// ローリングハッシュを生成する.
-// 2通りのハッシュを生成, 比較する.
-// 前計算O(|S|), 部分文字列のハッシュ取得定数時間.
-// @author Nauclhlt.
-public sealed class RabinKarp
+public struct RollingHash
 {
-    private const long BaseA = 3491;
-    private const long ModA = 481840747;
-    private const long BaseB = 8761;
-    private const long ModB = 999750347;
-
-
-    private long[] _prefixA;
-    private long[] _prefixB;
-    private long[] _powersA;
-    private long[] _powersB;
-    private string _source;
-
-    public int Length => _source.Length;
-    public string Source => _source;
-
-    // sのハッシュを前計算する.
-    // O(|s|)
-    public RabinKarp(string s)
-    {
-        _source = s;
-
-        int length = s.Length;
-        ReadOnlySpan<char> span = s.AsSpan();
-
-        _prefixA = new long[length + 1];
-        _powersA = new long[length + 1];
-        _powersA[0] = 1;
-
-        _prefixB = new long[length + 1];
-        _powersB = new long[length + 1];
-        _powersB[0] = 1;
-
-        for (int i = 0; i < length; i++)
-        {
-            long c = (long)span[i];
-            _prefixA[i + 1] = (_prefixA[i] * BaseA + c) % ModA;
-            _prefixB[i + 1] = (_prefixB[i] * BaseB + c) % ModB;
-            _powersA[i + 1] = (_powersA[i] * BaseA) % ModA;
-            _powersB[i + 1] = (_powersB[i] * BaseB) % ModB;
-        }
-    }
-
-    // [l, r)の範囲の部分文字列に対するハッシュを取得する.
-    // O(1)
-    public RollingHash GetHash(int l, int r)
-    {
-        if (r < l) throw new ArgumentException("! left <= right");
-
-        long hashA = (_prefixA[r] - _powersA[r - l] * _prefixA[l]) % ModA;
-        long hashB = (_prefixB[r] - _powersB[r - l] * _prefixB[l]) % ModB;
-        if (hashA < 0) hashA += ModA;
-        if (hashB < 0) hashB += ModB;
-
-        return new RollingHash(r - l, hashA, hashB);
-    }
-
-    // ハッシュaとbをこの順に結合したハッシュを返す.
-    // O(1)
-    public RollingHash Concat(RollingHash a, RollingHash b)
-    {
-        long destA = (_powersA[b.Length] * a.HashA + b.HashA) % ModA;
-        long destB = (_powersB[b.Length] * a.HashB + b.HashB) % ModB;
-        if (destA < 0) destA += ModA;
-        if (destB < 0) destB += ModB;
-
-        return new RollingHash(a.Length + b.Length, destA, destB);
-    }
-}
-
-public readonly struct RollingHash : IEquatable<RollingHash>
-{
-    private readonly long _hashA;
-    private readonly long _hashB;
-    private readonly int _length;
+    private long _hashA;
+    private long _hashB;
+    private int _length;
 
     public long HashA => _hashA;
     public long HashB => _hashB;
     public int Length => _length;
 
-    public RollingHash(int length, long hashA, long hashB)
+    public static RollingHash Zero => new(0, 0, 0);
+
+    public RollingHash()
     {
-        _length = length;
-        _hashA = hashA;
-        _hashB = hashB;
+        _hashA = 0;
+        _hashB = 0;
+        _length = 0;
     }
 
-    public bool Equals(RollingHash other)
+    public RollingHash(long hashA, long hashB, int length)
     {
-        return _length == other._length && _hashA == other._hashA && _hashB == other._hashB;
+        _hashA = hashA;
+        _hashB = hashB;
+        _length = length;
+    }
+
+    public static bool operator ==(RollingHash left, RollingHash right)
+    {
+        return left._hashA == right._hashA && left._hashB == right._hashB && left._length == right._length;
+    }
+
+    public static bool operator !=(RollingHash left, RollingHash right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return (_hashA, _hashB).GetHashCode();
     }
 
     public override bool Equals([NotNullWhen(true)] object obj)
     {
         if (obj is RollingHash other)
         {
-            return this.Equals(other);
+            return this == other;
         }
         else
+        {
             return false;
+        }
     }
 
-    public override int GetHashCode() => (_length, _hashA, _hashB).GetHashCode();
-
-    public static bool operator ==(RollingHash a, RollingHash b)
+    public static RollingHash FromChar(char c)
     {
-        return a.Equals(b);
+        return new (c, c, 1);
+    }
+}
+
+public static class RabinKarp
+{
+    public const long BaseA = 3491;
+    public const long ModA = 481840747;
+    public const long BaseB = 8761;
+    public const long ModB = 999750347;
+
+    private static long[] _powerA;
+    private static long[] _powerB;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long SafeMod(long a, long m)
+    {
+        return ((a % m) + m) % m;
     }
 
-    public static bool operator !=(RollingHash a, RollingHash b)
+    public static void Setup(int maxLength)
     {
-        return !a.Equals(b);
+        _powerA = new long[maxLength + 1];
+        _powerB = new long[maxLength + 1];
+        _powerA[0] = 1L;
+        _powerB[0] = 1L;
+        for (int i = 1; i <= maxLength; i++)
+        {
+            _powerA[i] = (_powerA[i - 1] * BaseA) % ModA;
+            _powerB[i] = (_powerB[i - 1] * BaseB) % ModB;
+        }
     }
 
-    public override string ToString()
+    public static RollingHash PrefixDiff(RollingHash left, RollingHash right)
     {
-        return $"{_length} {{{_hashA}, {_hashB}}}";
+        int diff = right.Length - left.Length;
+        long hashA = SafeMod(right.HashA - (left.HashA * _powerA[diff]) % ModA, ModA);
+        long hashB = SafeMod(right.HashB - (left.HashB * _powerB[diff]) % ModB, ModB);
+
+        return new(hashA, hashB, diff);
+    }
+
+    public static RollingHash Concat(RollingHash left, RollingHash right)
+    {
+        int len = left.Length + right.Length;
+        long hashA = ((left.HashA * _powerA[right.Length]) % ModA + right.HashA) % ModA;
+        long hashB = ((left.HashB * _powerB[right.Length]) % ModB + right.HashB) % ModB;
+        return new(hashA, hashB, len);
+    }
+
+    public static RollingHash HashFromString(string s)
+    {
+        long hashA = 0L;
+        long hashB = 0L;
+        for (int i = 0; i < s.Length; i++)
+        {
+            hashA = (hashA * BaseA) % ModA;
+            hashA += s[i];
+            hashA %= ModA;
+
+            hashB = (hashB * BaseB) % ModB;
+            hashB += s[i];
+            hashB %= ModB;
+        }
+
+        return new(hashA, hashB, s.Length);
+    }
+
+    public static long PowerA(int exp)
+    {
+        return _powerA[exp];
+    }
+
+    public static long PowerB(int exp)
+    {
+        return _powerB[exp];
+    }
+}
+
+public sealed class RollingHashString
+{
+    private int _length;
+    private string _source;
+    private RollingHash[] _prefix;
+
+    public int Length => _length;
+    public string Source => _source;
+
+    public RollingHashString(string source)
+    {
+        _source = source;
+        _length = source.Length;
+
+        _prefix = new RollingHash[_length + 1];
+        _prefix[0] = RollingHash.Zero;
+        for (int i = 1; i <= _length; i++)
+        {
+            _prefix[i] = RabinKarp.Concat(_prefix[i - 1], RollingHash.FromChar(_source[i - 1]));
+        }
+    }
+
+    public RollingHash GetHash(int l, int r)
+    {
+        return RabinKarp.PrefixDiff(_prefix[l], _prefix[r]);
+    }
+
+    public RollingHash GetPrefixHash(int length)
+    {
+        return _prefix[length];
     }
 }
